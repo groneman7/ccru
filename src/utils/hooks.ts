a0e60f434afd556451188ea4e7a834179ff0152f
Evan Groneman <groneman7@gmail.com>
2024-07-30 22:58:12 -0400

    Add timeout and debounce hooks

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 export function useKeyDown(callbackFn: () => void, keys: string[]) {
   const onKeyDown = useCallback((e: KeyboardEvent) => {
@@ -17,4 +17,39 @@ export function useKeyDown(callbackFn: () => void, keys: string[]) {
       document.removeEventListener('keydown', onKeyDown)
     }
   }, [onKeyDown])
+}
+
+export function useTimeout(callbackFn: () => unknown, delay: number) {
+  const callbackRef = useRef(callbackFn);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    callbackRef.current = callbackFn
+  }, [callbackFn])
+  
+  const set = useCallback(() => {
+    timeoutRef.current = setTimeout(() => callbackRef.current(), delay)
+  }, [delay])
+
+  const clear = useCallback(() => {
+    timeoutRef.current && clearTimeout(timeoutRef.current)
+  }, [])
+
+  useEffect(() => {
+    set()
+    return clear
+  }, [delay, set, clear])
+
+  const reset = useCallback(() => {
+    clear()
+    set()
+  }, [clear, set])
+
+  return { reset, clear}
+}
+
+export function useDebounce(callbackFn: () => unknown, delay: number, dependencies: unknown[]) {
+  const { reset, clear } = useTimeout(callbackFn, delay)
+  useEffect(reset, [...dependencies, reset])
+  useEffect(clear, [])
 }
