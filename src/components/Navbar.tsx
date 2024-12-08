0cf5bd59fc1090b6e3c3befc38b3649cea8cbf3a
Evan Groneman <groneman7@gmail.com>
2024-12-08 11:53:22 -0500

    Add initial home layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { Button } from "./ui/button";
+
+function AdminButton() {
+    return (
+        <Link href="/admin">
+            <Button>Administrator Settings</Button>
+        </Link>
+    );
+}
+
+export default async function Navbar() {
+    const user = await currentUser();
+
+    return (
+        <nav className="flex items-center gap-4 border-b p-4">
+            <div className="flex basis-1/4 items-center justify-start">
+                <h1 className="text-xl font-bold">Compassionate Care Reaching yoU</h1>
+            </div>
+            <div className="flex flex-1 items-center justify-center gap-4">
+                <Link href="/events">
+                    <Button>Events</Button>
+                </Link>
+                <Link href="">
+                    <Button>About us</Button>
+                </Link>
+                <Link href="">
+                    <Button>Contact</Button>
+                </Link>
+            </div>
+            <div className="flex basis-1/4 items-center justify-end">
+                <SignedOut>
+                    <SignInButton>
+                        <Button>Volunteer Portal</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <div className="flex items-center gap-4">
+                        {user?.privateMetadata?.roleId === "669c092236951612dac7c52a" ? (
+                            <AdminButton />
+                        ) : null}
+                        <UserButton />
+                    </div>
+                </SignedIn>
+            </div>
+        </nav>
+    );
+}
