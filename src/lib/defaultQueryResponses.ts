b3ac525e7e6e0a4eb2569012bb01a9177b0e106c
Evan Groneman <groneman7@gmail.com>
2025-02-02 16:57:11 -0500

    Update type logic; return functions for reponses

--- src/lib/defaultQueryResponses.ts
+++ src/lib/defaultQueryResponses.ts
@@ -1,24 +1,56 @@
-export type QueryResponse<T = undefined> = {
-    status: number;
-    message?: string;
-    data?: T;
+export type SuccessResponse<T> = {
+    status: 200 | 201;
+    message: string;
+    data: T;
 };
 
-export const defaultQueryResponses = {
-    200: {
-        status: 200,
-        message: "OK",
-    },
-    201: {
-        status: 201,
-        message: "Created",
-    },
-    404: {
-        status: 404,
-        message: "Not found",
-    },
-    500: {
-        status: 500,
-        message: "Internal server error",
-    },
+export type ErrorResponse = {
+    status: 400 | 403 | 404 | 500;
+    message: string;
 };
+
+export type QueryResponse<T> = SuccessResponse<T> | ErrorResponse;
+
+export function ok<T>(data: T, message?: string): SuccessResponse<T> {
+    return {
+        status: 200,
+        message: message ?? "OK",
+        data,
+    };
+}
+
+export function created<T>(data: T, message?: string): QueryResponse<T> {
+    return {
+        status: 201,
+        message: message ?? "Created",
+        data,
+    };
+}
+
+export function badRequest(message?: string): ErrorResponse {
+    return {
+        status: 400,
+        message: message ?? "Bad request",
+    };
+}
+
+export function notFound(message?: string): ErrorResponse {
+    return {
+        status: 404,
+        message: message ?? "Not found",
+    };
+}
+
+export function forbidden(message?: string): ErrorResponse {
+    return {
+        status: 403,
+        message: message ?? "Forbidden",
+    };
+}
+
+export function internalServerError(message?: string): ErrorResponse {
+    return {
+        status: 500,
+        message: message ?? "Internal server error",
+    };
+}
