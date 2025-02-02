6eefeb5f18ffbc36274b26447afe498e87159a9c
Evan Groneman <groneman7@gmail.com>
2025-02-01 21:35:34 -0500

    Use client side event form

--- src/app/events/new/page.tsx
+++ src/app/events/new/page.tsx
@@ -1,8 +1,14 @@
+import NewEventForm from "~/components/NewEventForm";
+import { getAllPositions } from "~/prisma/events";
 
-export default function Page() {
+export default async function Page() {
+    const { data: positions } = await getAllPositions();
+    console.log(positions);
+
     return (
-        <div>
-            New event form
+        <div className="flex flex-col gap-6 p-4">
+            <span className="text-2xl font-semibold">Create a new event</span>
+            <NewEventForm positionList={positions || []} />
         </div>
     );
 }
