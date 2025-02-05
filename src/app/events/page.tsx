97170fe52893b0f99c15a3c2b3be853165d5bf80
Evan Groneman <groneman7@gmail.com>
2025-02-05 16:43:09 -0500

    Make event calendar its own page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,16 @@
-import { permanentRedirect } from "next/navigation";
-import dayjs from "dayjs";
+import { EventCalendar } from "~/components/EventCalendar";
+import { getEvents } from "~/prisma/events";
 
 // Events Page
-export default function Page() {}
+export default async function Page() {
+    const { data: events } = await getEvents().then((res) => res);
+
+    return (
+        <div className="flex flex-1">
+            <EventCalendar
+                canEdit
+                events={events}
+            />
+        </div>
+    );
+}
