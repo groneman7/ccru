de1ad218c731a53a1ca29fa1b1ca485bd5f5caa0
Evan Groneman <groneman7@gmail.com>
2025-04-20 15:09:44 -0400

    upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+import tseslint from "typescript-eslint";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: import.meta.dirname,
+});
+
+export default tseslint.config(
+    {
+        ignores: [".next"],
+    },
+    ...compat.extends("next/core-web-vitals"),
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        extends: [
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        rules: {
+            "@typescript-eslint/array-type": "off",
+            "@typescript-eslint/consistent-type-definitions": "off",
+            "@typescript-eslint/consistent-type-imports": [
+                "warn",
+                {
+                    prefer: "type-imports",
+                    fixStyle: "inline-type-imports",
+                },
+            ],
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
+            "@typescript-eslint/require-await": "off",
+            "@typescript-eslint/no-misused-promises": [
+                "error",
+                {
+                    checksVoidReturn: {
+                        attributes: false,
+                    },
+                },
+            ],
+        },
+    },
+    {
+        linterOptions: {
+            reportUnusedDisableDirectives: true,
+        },
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+    }
+);
